d642ab35ff351a6354360bf9d32cd557b3c3cd19
luyoulun <luyoulun@wxchina.com>
2016-11-04 17:30:48 +0800

    commit

---
+++ app/src/main/java/com/puzzle/jigsaw/GameApplication.java
@@ -0,0 +1,14 @@
+package com.puzzle.jigsaw;
+
+import android.app.Application;
+
+/**
+ * Created by Amberllo on 2016/11/4.
+ */
+public class GameApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        TypefaceProvider.registerDefaultIconSets();
+    }
+}
--- app/src/main/java/com/puzzle/jigsaw/MainActivity.java
+++ app/src/main/java/com/puzzle/jigsaw/MainActivity.java
@@ -1,26 +1,56 @@
 package com.puzzle.jigsaw;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.f2prateek.rx.preferences.Preference;
+import com.f2prateek.rx.preferences.RxSharedPreferences;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.nereo.multi_image_selector.MultiImageSelector;
+
 public class MainActivity extends AppCompatActivity {
-    GameView gameView;
-    Bitmap bitmap;
+
+    int REQUEST_CODE_PICK_IMAGE = 1024;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gameView.setBitmap(bitmap);
-            }
-        });
-        gameView = (GameView)findViewById(R.id.gameView);
-        bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.img);
-        gameView.setBitmap(bitmap);
+        ButterKnife.bind(this);
+
+
     }
+
+    @OnClick(R.id.btn_select)
+    protected void getImageFromAlbum() {
+//        Intent intent = new Intent(Intent.ACTION_PICK);
+//        intent.setType("image/*");//相片类型
+//        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+
+        // Multi image selector form an Activity
+        MultiImageSelector.create(this)
+                .single()
+                .start(this, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    /**
+     * 设置游戏难度
+     * */
+    @OnClick(R.id.btn_mode)
+    protected void setMode() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        RxSharedPreferences rxPreferences = RxSharedPreferences.create(preferences);
+        Preference<String> username = rxPreferences.getString("username");
+        Preference<Boolean> showWhatsNew = rxPreferences.getBoolean("show-whats-new", true);
+    }
+
 }
